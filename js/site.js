5959b3d63cb6c79ce85fa8b71ea37c407a8c1a81
Muhammad Azmain Mahtab <azmainmah@gmail.com>
2023-05-24 22:10:33 +0600

    Major Design Changes

--- js/site.js
+++ js/site.js
@@ -1,13 +1,36 @@
+window.addEventListener("scroll", function () {
+    var nav = document.querySelector(".navbar");
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    if (window.pageYOffset > 0) {
+        nav.classList.add("shadow");
+        scrollToTopBtn.classList.add("show");
+    } else {
+        nav.classList.remove("shadow");
+        scrollToTopBtn.classList.remove("show");
+    }
+});
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+            entry.target.classList.add('show1');
         } else {
             entry.target.classList.remove('show');
+            entry.target.classList.remove('show1');
         }
     });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElements1 = document.querySelectorAll('.hidden1');
+hiddenElements1.forEach((el) => observer.observe(el));
